9bb8179c8af7bc39325dc97628924e45b87cabcc
Uğur Yürümez <73554899+uqursuz0@users.noreply.github.com>
2024-03-23 05:18:59 +0300

    bitirme.py

--- bitirme.py
+++ bitirme.py
@@ -1,47 +1,61 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import folium
+from geopy.geocoders import Nominatim
 
+# Geolocator oluşturma
+geolocator = Nominatim(user_agent="streamlit_app")
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 # Kargo şubeleri, iller ve ilçeleri içeren bir sözlük
-cargo_data = {
-    "Şube 1": {
-        "İstanbul": {
-            "Beşiktaş": {"Tahmini Kargo": 100, "Tahmini Gelir": 5000},
-            "Kadıköy": {"Tahmini Kargo": 150, "Tahmini Gelir": 7000},
-            "Şişli": {"Tahmini Kargo": 120, "Tahmini Gelir": 6000}
-        },
-        "Ankara": {
-            "Çankaya": {"Tahmini Kargo": 80, "Tahmini Gelir": 4000},
-            "Keçiören": {"Tahmini Kargo": 90, "Tahmini Gelir": 4500},
-            "Yenimahalle": {"Tahmini Kargo": 100, "Tahmini Gelir": 4800}
-        },
-        "İzmir": {
-            "Konak": {"Tahmini Kargo": 110, "Tahmini Gelir": 5500},
-            "Karşıyaka": {"Tahmini Kargo": 100, "Tahmini Gelir": 5000},
-            "Bornova": {"Tahmini Kargo": 120, "Tahmini Gelir": 6000}
-        }
-    }
-}
+cargo_data = pd.read_csv("bolge.csv",header=0,parse_dates=["datedelivery"])
 
 # Ana sayfa
 def main():
-    st.title("Kargo Şube ve İl Seçimi")
-    selected_branch, selected_city, selected_district = get_user_selection()
+    st.title("İlgili Ay Tahmin Verileri")
+    selected_city, selected_district = get_user_selection()
     if selected_district:
-        display_results(selected_branch, selected_city, selected_district)
+        display_results( selected_city, selected_district)
 
 # Kullanıcı seçimlerini al
 def get_user_selection():
-    selected_branch = st.sidebar.selectbox("Kargo Şubesi Seçin", list(cargo_data.keys()))
-    selected_city = st.sidebar.selectbox("İl Seçin", list(cargo_data[selected_branch].keys()))
-    selected_district = st.sidebar.selectbox("İlçe Seçin", list(cargo_data[selected_branch][selected_city].keys()))
-    return selected_branch, selected_city, selected_district
+    selected_city = st.sidebar.selectbox("İl Seçin", ["İstanbul"])  
+    #selected_city = st.sidebar.selectbox("İl Seçin", cargo_data["City"].drop_duplicates().tolist())
+    selected_district = st.sidebar.selectbox("İlçe Seçin", cargo_data[cargo_data["City"]==selected_city]["State"].drop_duplicates().tolist())
+    return  selected_city, selected_district
 
 # Sonuçları görüntüle
-def display_results(selected_branch, selected_city, selected_district):
-    st.subheader(f"Seçilen İl: {selected_city}, Seçilen İlçe: {selected_district}")
-    st.subheader("Kargo Şubesi Tahmini Kargoları")
-    df = pd.DataFrame(cargo_data[selected_branch][selected_city][selected_district], index=["Tahmini Değerler"])
-    st.write(df)
+def display_results( selected_city, selected_district):
+    current_month = datetime.now().month -1 
+    #st.subheader(f"Seçilen İl: {selected_city}, Seçilen İlçe: {selected_district}")
+    df_filter = cargo_data[(cargo_data["City"]==selected_city) & (cargo_data["State"]==selected_district)]
+    df = df_filter[["NumberOfCargo","datedelivery"]]
+    df=df[df["datedelivery"].dt.month == current_month]
+    df = df.sort_values(by="datedelivery")
+    df=df.set_index("datedelivery")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.dataframe(df, height=900, width=300)
+    with right_column:
+        fig, ax = plt.subplots(figsize=(4, 4)) 
+        ax.plot(df)
+        st.pyplot(fig)
+    
+    location = geolocator.geocode(selected_district)
+    if location:
+        selected_lat, selected_lon = location.latitude, location.longitude
+    else:
+        st.error("Koordinatlar bulunamadı")
 
+    # Harita oluşturma
+    data = pd.DataFrame({
+    'lat': [selected_lat],
+    'lon': [selected_lon]
+    })
+
+    st.map(data)
 if __name__ == "__main__":
     main()
